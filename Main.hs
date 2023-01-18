1672635d8ab76f99870b1d176be0023336853905
HomelikeBrick42 <64717463+HomelikeBrick42@users.noreply.github.com>
2023-01-18 16:28:12 +1300

    Added randomness and multisampling

--- Main.hs
+++ Main.hs
@@ -1,5 +1,7 @@
 import Data.Maybe (fromMaybe)
+import GHC.IO (unsafePerformIO)
 import Graphics.Image
+import System.Random (Random (random, randoms), RandomGen, mkStdGen, randomIO)
 import Vector3
 
 data Ray = Ray
@@ -7,11 +9,17 @@ data Ray = Ray
     rayDirection :: Vector3 Double
   }
 
+data Material = Material
+  { materialColor :: Vector3 Double,
+    materialEmit :: Vector3 Double,
+    materialReflect :: Double
+  }
+
 data Hit = Hit
   { hitPosition :: Vector3 Double,
     hitNormal :: Vector3 Double,
     hitDistance :: Double,
-    hitColor :: Vector3 Double
+    hitMaterial :: Material
   }
 
 class Intersect a where
@@ -26,14 +34,14 @@ instance Intersect Intersectable where
 data Sphere = Sphere
   { sphereCenter :: Vector3 Double,
     sphereRadius :: Double,
-    sphereColor :: Vector3 Double
+    sphereMaterial :: Material
   }
 
 instance Intersect Sphere where
-  intersect (Ray origin direction) (Sphere center radius color) =
-    if discriminant < 0 || distance < 0
+  intersect (Ray origin direction) (Sphere center radius material) =
+    if (discriminant < 0) || (distance <= 0)
       then Nothing
-      else Just $ Left $ Hit position normal distance color
+      else Just $ Left $ Hit position normal distance material
     where
       oc = origin - center
       a = sqrLengthVector3 direction
@@ -43,14 +51,14 @@ instance Intersect Sphere where
       distance =
         let t0 = (-half_b - sqrt discriminant) / a
             t1 = (-half_b + sqrt discriminant) / a
-         in if t0 > 0
+         in if (t0 < t1) && (t0 > 0)
               then t0
               else t1
       position = origin + direction * toVector3 distance
       normal = (position - center) / toVector3 radius
 
-trace :: [Intersectable] -> Ray -> Maybe Hit
-trace objects ray = foldr (nearest . intersect ray) Nothing objects
+rayCast :: [Intersectable] -> Ray -> Maybe Hit
+rayCast objects ray = foldr (nearest . intersect ray) Nothing objects
   where
     nearest :: Maybe (Either Hit Ray) -> Maybe Hit -> Maybe Hit
     nearest Nothing b = b
@@ -60,7 +68,42 @@ trace objects ray = foldr (nearest . intersect ray) Nothing objects
         if hitDistance a < hitDistance b
           then a
           else b
-    nearest (Just (Right a)) b = nearest (Left <$> trace objects a) b
+    nearest (Just (Right a)) b = nearest (Left <$> rayCast objects a) b
+
+rayTrace :: forall tg. RandomGen tg => tg -> [Intersectable] -> Ray -> Int -> Int -> (Vector3 Double, tg)
+rayTrace rng objects ray maxDepth samplesPerBounce = trace rng ray maxDepth
+  where
+    trace :: tg -> Ray -> Int -> (Vector3 Double, tg)
+    trace rng _ 0 = (Vector3 0 0 0, rng)
+    trace rng ray depth
+      | Just hit <- rayCast objects ray =
+          let (color, rng') = sample rng hit depth samplesPerBounce
+           in (color / toVector3 (fromIntegral samplesPerBounce), rng')
+    trace rng ray _ = (skyColor $ rayDirection ray, rng)
+
+    sample :: tg -> Hit -> Int -> Int -> (Vector3 Double, tg)
+    sample rng hit depth 0 = (Vector3 0 0 0, rng)
+    sample rng hit depth 1 = bounce rng hit depth
+    sample rng hit depth samplesLeft =
+      let (color, rng') = bounce rng hit depth
+          (new_color, rng'') = sample rng' hit depth (samplesLeft - 1)
+       in (color + new_color, rng'')
+
+    bounce :: tg -> Hit -> Int -> (Vector3 Double, tg)
+    bounce rng hit depth =
+      let (randomDirection, rng') = randomVector3InDirection rng (hitNormal hit)
+          material = hitMaterial hit
+          ray' =
+            Ray
+              { rayOrigin = rayOrigin ray,
+                rayDirection =
+                  lerpVector3
+                    randomDirection
+                    (reflectVector3 (rayDirection ray) (hitNormal hit))
+                    (materialReflect material)
+              }
+          (color, rng'') = trace rng' ray' (depth - 1)
+       in (materialEmit material + materialColor material * color, rng'')
 
 skyColor :: Vector3 Double -> Vector3 Double
 skyColor direction = lerpVector3 downColor upColor $ vector3y (normalizeVector3 direction) * 0.5 + 0.5
@@ -80,15 +123,15 @@ pixelColor
   objects
   (cameraPosition, Vector3 cameraRight cameraUp cameraForward)
   (width, height)
-  (x, y) = maybe (skyColor rayDirection) hitColor (trace objects ray)
+  (x, y) = fst $ rayTrace (mkStdGen (x + y * width)) objects ray 1000 100
     where
       aspect :: Double
       aspect = fromIntegral width / fromIntegral height
       uv :: Vector3 Double
       uv =
         Vector3
-          (fromIntegral x / fromIntegral width)
-          (fromIntegral y / fromIntegral height)
+          ((fromIntegral x + 0.5) / fromIntegral width)
+          ((fromIntegral y + 0.5) / fromIntegral height)
           0
       rayDirection :: Vector3 Double
       rayDirection =
@@ -107,14 +150,30 @@ image objects camera (width, height) = makeImageR VU (height, width) pixel
 
 main :: IO ()
 main =
-  let cameraPosition = Vector3 0 0 0
+  let cameraPosition = Vector3 0 1 (-3)
       cameraDirection = Vector3 (Vector3 1 0 0) (Vector3 0 1 0) (Vector3 0 0 1)
       objects =
         [ Intersectable
             Sphere
-              { sphereCenter = Vector3 0 0 3,
+              { sphereCenter = Vector3 0 1 0,
                 sphereRadius = 1,
-                sphereColor = Vector3 1 0 0
+                sphereMaterial =
+                  Material
+                    { materialColor = Vector3 0.8 0.8 0.8,
+                      materialEmit = Vector3 0 0 0,
+                      materialReflect = 0
+                    }
+              },
+          Intersectable
+            Sphere
+              { sphereCenter = Vector3 0 (-100000) 0,
+                sphereRadius = 100000,
+                sphereMaterial =
+                  Material
+                    { materialColor = Vector3 0.2 0.8 0.3,
+                      materialEmit = Vector3 0 0 0,
+                      materialReflect = 0
+                    }
               }
         ]
    in writeImage "image.png" $ image objects (cameraPosition, cameraDirection) (640, 480)
--- Vector3.hs
+++ Vector3.hs
@@ -1,5 +1,7 @@
 module Vector3 where
 
+import System.Random (Random (random), RandomGen)
+
 data Vector3 a = Vector3
   { vector3x :: a,
     vector3y :: a,
@@ -27,6 +29,30 @@ normalizeVector3 v = v / toVector3 (lengthVector3 v)
 
 lerpVector3 :: (Num a) => Vector3 a -> Vector3 a -> a -> Vector3 a
 lerpVector3 v1 v2 t = v1 * toVector3 (1 - t) + v2 * toVector3 t
+
+reflectVector3 :: (Num a) => Vector3 a -> Vector3 a -> Vector3 a
+reflectVector3 d n = d - (toVector3 (2 * dotVector3 d n) * n)
+
+randomVector3 :: forall tg. RandomGen tg => tg -> (Vector3 Double, tg)
+randomVector3 rng =
+  let (x, rng') = (random rng :: (Double, tg))
+      (y, rng'') = (random rng' :: (Double, tg))
+      (z, rng''') = (random rng'' :: (Double, tg))
+   in ( Vector3
+          (x * 2.0 - 1.0)
+          (y * 2.0 - 1.0)
+          (z * 2.0 - 1.0),
+        rng'''
+      )
+
+randomVector3InDirection :: forall tg. RandomGen tg => tg -> Vector3 Double -> (Vector3 Double, tg)
+randomVector3InDirection rng normal =
+  let (direction, rng') = randomVector3 rng
+   in ( if dotVector3 direction normal >= 0
+          then direction
+          else -direction,
+        rng'
+      )
 
 instance Num a => Num (Vector3 a) where
   (+) (Vector3 x1 y1 z1) (Vector3 x2 y2 z2) = Vector3 (x1 + x2) (y1 + y2) (z1 + z2)
